709adef2b3ad1ed0600b631ff33db432cc48fff5
권한준 <skclaqks11@naver.com>
2016-11-18 20:01:05 +0900

    Layout 호출되도록 할것

--- app/src/main/java/com/example/a403/favoriteanimal/MainActivity.java
+++ app/src/main/java/com/example/a403/favoriteanimal/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.a403.favoriteanimal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    CheckBox cb1 = (CheckBox)findViewById(R.id.checkBox);
+    View lay = findViewById(R.id.subLayout);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SetCheckBox();
+    }
+    void SetCheckBox(){
+        cb1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(cb1.isChecked()){
+                    lay.setVisibility(View.VISIBLE);
+                }
+                else{
+                    lay.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
     }
 }
